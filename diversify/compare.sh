04996cd0b3a03334051f429a5bfa2d593df7444e
Nicolas Petitprez <nicolas.petitprez@gmail.com>
2015-08-25 11:10:10 +0200

    add diversify profile and launch script

---
+++ diversify/compare.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+function test {
+    "$@"
+    local status=$?
+    if [ $status -ne 0 ]; then
+        echo "error with $1" >&2
+        exit;
+    fi
+    return $status
+}
+
+test cd ..
+test rm -rf generated
+
+echo "create baseline"
+# generate baseline
+test mvn clean compile -Dmaven.test.skip=true >> log.txt
+test mkdir generated
+test cp -r target/classes generated;
+
+processors=(
+    "none"
+    
+    "fr.inria.diversify.EncryptLiteralProcessor"
+    "fr.inria.diversify.InvertIfProcessor"
+    "fr.inria.diversify.VariableDeclaration"
+    "fr.inria.diversify.FantomProcessor"
+
+    "cohen.process.EmptyMethod"
+    "cohen.process.EquivalenceInstr"
+#    "cohen.process.IfEquivalence"
+#    "cohen.process.MethodTampon"
+#    "cohen.process.ReplaceVarDeclarationsProcessor"
+    "cohen.process.ReverseIf"
+    "cohen.process.ThreadCreationProcessor"
+ )
+
+for processor in "${processors[@]}"
+do
+    echo $processor
+    # generate artifact to compare
+    test mvn clean compile -Dmaven.test.skip=true -Pdiversify -Dprocessor=$processor >> log.txt
+
+    # compute similarity
+    test java -jar diversify/Diffcompule-1.0-SNAPSHOT-jar-with-dependencies.jar generated/classes  target/classes
+done
